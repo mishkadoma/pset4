1d679568e2796663f2e86c26a2bf9269277c3697
mishkadoma <mixandoma@ya.ru>
2017-02-17 21:54:27 +0500

    initial specs implemented

--- recover/recover.c
+++ recover/recover.c
@@ -2,5 +2,17 @@
 
 int main(int argc, char *argv[])
 {
-  
+  if (argc != 2)
+  {
+    fprintf(stderr, "Usare: ./recover infile")
+    return 1;
+  }
+
+  char *infile = argv[1];
+
+  FILE *input = fopen(infile, "r");
+  if (input == NULL)
+  {
+    fprintf(stderr, "couldn't open %s.\n", infile)
+  }
 }
