95eac7a61e111b4ca59b2969a4f6efb340cc7447
mishkadoma <mixandoma@ya.ru>
2017-02-25 04:21:13 +0500

    added files opening

---
+++ resize/resize.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int int main(int argc, char *argv[]) {
+  if (argc != 3)
+  {
+    printf("Usage: ./resize small.bmp large.bmp");
+    return 1;
+  }
+
+  char *small = argc[1];
+  char *large = argc[2];
+
+
+//open both files;
+  FILE *inprt = fopen(small, "r");
+  if (inptr == NULL)
+  {
+    fprintf(stderr, "Couldn't open %s\n", *small);
+    return 2;
+  }
+
+  FILE *outptr = fopen(large, "w");
+  if (outptr == NULL)
+  {
+    fprintf(stderr, "Couldn't open %s\n", *large);
+    return 3;
+  }
+  
+
+
+  return 0;
+}
