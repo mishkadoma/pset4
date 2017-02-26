20bbf9bc672060109f305f7ef2e39b711cf8022a
mishkadoma <mixandoma@ya.ru>
2017-02-26 20:03:39 +0500

    triple copying

--- resize/resize.c
+++ resize/resize.c
@@ -4,14 +4,16 @@
 #include "bmp.h"
 
 int main(int argc, char *argv[]) {
-  if (argc != 3)
+  if (argc != 4)
   {
-    printf("Usage: ./resize small.bmp large.bmp\n");
+    printf("Usage: ./resize n small.bmp large.bmp\n");
     return 1;
   }
 
-  char *small = argv[1];
-  char *large = argv[2];
+
+  char *multiplier = argv[1];
+  char *small = argv[2];
+  char *large = argv[3];
 
 
 //open both files;
@@ -44,11 +46,31 @@ int main(int argc, char *argv[]) {
       return 4;
   }
 
-  fwrite(&bf, BITMAPFILEHEADER, 1, larptr);
+  fwrite(&bf, sizeof(BITMAPFILEHEADER), 1, larptr);
 
-  fwrite(&bf, BITMAPINFOHEADER, 1, larptr);
+  fwrite(&bf, sizeof(BITMAPINFOHEADER), 1, larptr);
 
-  
+  int padding = (4 - (bi.biWidth * sizeof(RGBTRIPLE)) % 4) % 4;
+
+  for (int i = 0, biHeight = abs(bi.biHeight); i < biHeight; i++)
+  {
+      for (int j = 0; j < bi.biWidth; j++)
+      {
+          RGBTRIPLE triple;
+
+          fread(&triple, sizeof(RGBTRIPLE), 1, smptr);
+
+          for (int k = 0, m = atoi(multiplier); k < m; k++)
+            fwrite(&triple, sizeof(RGBTRIPLE), 1, larptr);
+      }
+
+      fseek(smptr, padding, SEEK_CUR);
+
+      for (int k = 0; k < padding; k++)
+          fputc(0x00, larptr);
+  }
+
+
 
 
   return 0;
